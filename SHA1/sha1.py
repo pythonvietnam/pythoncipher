638a56c890f566aca0b251949a8fb889b489c89e
namtranvn <33787604+namtranvn@users.noreply.github.com>
2018-07-26 21:51:46 +0900

    Update sha1.py

--- SHA1/sha1.py
+++ SHA1/sha1.py
@@ -1,161 +1,68 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-# SHA1.py
-# ----------------------------------------------------------------------
+def sha1(data):
+    bytes = ""
 
-import sys
+    h0 = 0x67452301
+    h1 = 0xEFCDAB89
+    h2 = 0x98BADCFE
+    h3 = 0x10325476
+    h4 = 0xC3D2E1F0
+    
+    for n in range(len(data)):
+        bytes+='{0:08b}'.format(ord(data[n]))
+    bits = bytes+"1"
+    pBits = bits
+    #pad until length equals 448 mod 512
+    while len(pBits)%512 != 448:
+        pBits+="0"
+    #append the original length
+    pBits+='{0:064b}'.format(len(bits)-1)
 
-class SHA1:
-    def __init__(self):
-        self.__H = [
-            0x67452301,
-            0xEFCDAB89,
-            0x98BADCFE,
-            0x10325476,
-            0xC3D2E1F0
-            ]
+    def chunks(l, n):
+        return [l[i:i+n] for i in range(0, len(l), n)]
 
-    def __str__(self):
-        return ''.join((hex(h)[2:]).rjust(8, '0') for h in self.__H)
+    def rol(n, b):
+        return ((n << b) | (n >> (32 - b))) & 0xffffffff
+    
+    for c in chunks(pBits, 512): 
+        words = chunks(c, 32)
+        w = [0]*80
+        for n in range(0, 16):
+            w[n] = int(words[n], 2)
+        for i in range(16, 80):
+            w[i] = rol((w[i-3] ^ w[i-8] ^ w[i-14] ^ w[i-16]), 1)  
 
-    # Private static methods used for internal operations.
-    @staticmethod
-    def __ROTL(n, x, w=32):
-        return ((x << n) | (x >> w - n))
-
-    @staticmethod
-    def __padding(stream):
-        l = len(stream)  # Bytes
-        hl = [int((hex(l*8)[2:]).rjust(16, '0')[i:i+2], 16)
-              for i in range(0, 16, 2)]
-
-        l0 = (56 - l) % 64
-        if not l0:
-            l0 = 64
-
-        if isinstance(stream, str):
-            stream += chr(0b10000000)
-            stream += chr(0)*(l0-1)
-            for a in hl:
-                stream += chr(a)
-        elif isinstance(stream, bytes):
-            stream += bytes([0b10000000])
-            stream += bytes(l0-1)
-            stream += bytes(hl)
-
-        return stream
-
-    @staticmethod
-    def __prepare(stream):
-        M = []
-        n_blocks = len(stream) // 64
-
-        stream = bytearray(stream)
-
-        for i in range(n_blocks):  # 64 Bytes per Block
-            m = []
-
-            for j in range(16):  # 16 Words per Block
-                n = 0
-                for k in range(4):  # 4 Bytes per Word
-                    n <<= 8
-                    n += stream[i*64 + j*4 + k]
-
-                m.append(n)
-
-            M.append(m[:])
-
-        return M
-
-    @staticmethod
-    def __debug_print(t, a, b, c, d, e):
-        print('t = {0} : \t'.format(t),
-              (hex(a)[2:]).rjust(8, '0'),
-              (hex(b)[2:]).rjust(8, '0'),
-              (hex(c)[2:]).rjust(8, '0'),
-              (hex(d)[2:]).rjust(8, '0'),
-              (hex(e)[2:]).rjust(8, '0')
-              )
-
-    # Private instance methods used for internal operations.
-    def __process_block(self, block):
-        MASK = 2**32-1
-
-        W = block[:]
-        for t in range(16, 80):
-            W.append(SHA1.__ROTL(1, (W[t-3] ^ W[t-8] ^ W[t-14] ^ W[t-16]))
-                     & MASK)
-
-        a, b, c, d, e = self.__H[:]
-
-        for t in range(80):
-            if t <= 19:
-                K = 0x5a827999
-                f = (b & c) ^ (~b & d)
-            elif t <= 39:
-                K = 0x6ed9eba1
+        a = h0
+        b = h1
+        c = h2
+        d = h3
+        e = h4
+        #Main loop
+        for i in range(0, 80):
+            if 0 <= i <= 19:
+                f = (b & c) | ((~b) & d)
+                k = 0x5A827999
+            elif 20 <= i <= 39:
                 f = b ^ c ^ d
-            elif t <= 59:
-                K = 0x8f1bbcdc
-                f = (b & c) ^ (b & d) ^ (c & d)
-            else:
-                K = 0xca62c1d6
+                k = 0x6ED9EBA1
+            elif 40 <= i <= 59:
+                f = (b & c) | (b & d) | (c & d) 
+                k = 0x8F1BBCDC
+            elif 60 <= i <= 79:
                 f = b ^ c ^ d
+                k = 0xCA62C1D6
 
-            T = ((SHA1.__ROTL(5, a) + f + e + K + W[t]) & MASK)
+            temp = rol(a, 5) + f + e + k + w[i] & 0xffffffff
             e = d
             d = c
-            c = SHA1.__ROTL(30, b) & MASK
+            c = rol(b, 30)
             b = a
-            a = T
+            a = temp
 
-            #SHA1.debug_print(t, a,b,c,d,e)
+        h0 = h0 + a & 0xffffffff
+        h1 = h1 + b & 0xffffffff
+        h2 = h2 + c & 0xffffffff
+        h3 = h3 + d & 0xffffffff
+        h4 = h4 + e & 0xffffffff
+    return '%08x%08x%08x%08x%08x' % (h0, h1, h2, h3, h4)
 
-        self.__H[0] = (a + self.__H[0]) & MASK
-        self.__H[1] = (b + self.__H[1]) & MASK
-        self.__H[2] = (c + self.__H[2]) & MASK
-        self.__H[3] = (d + self.__H[3]) & MASK
-        self.__H[4] = (e + self.__H[4]) & MASK
-
-    # Public methods for class use.
-    def update(self, stream):
-        stream = SHA1.__padding(stream)
-        stream = SHA1.__prepare(stream)
-
-        for block in stream:
-            self.__process_block(block)
-
-    def digest(self):
-        pass
-
-    def hexdigest(self):
-        s = ''
-        for h in self.__H:
-            s += (hex(h)[2:]).rjust(8, '0')
-        return s
-
-def usage():
-    print('Usage: python SHA1.py <file> [<file> ...]')
-    sys.exit()
-
-def main():
-
-    if len(sys.argv) < 2:
-        usage()
-
-    for filename in sys.argv[1:]:
-        try:
-            with open(filename, 'rb') as f:
-                content = f.read()
-
-        except:
-            print ('ERROR: Input file "{0}" cannot be read.'.format(filename))
-
-        else:
-            h = SHA1()
-            h.update(content)
-            hex_sha = h.hexdigest()
-            print("{0}  {1}".format(hex_sha, filename))
-
-if __name__ == '__main__':
-    main()
+print sha1("hello world")
